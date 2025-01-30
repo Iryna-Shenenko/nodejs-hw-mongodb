c235d2142cfdc80d7f749b76688f2ca86c44a862
Iryna Shenenko <163681074+Iryna-Shenenko@users.noreply.github.com>
2025-01-29 21:35:50 -0700

    index file

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,18 +1,23 @@
 import { Router } from "express";
-import  {getContactByIdController, getContactsController, createContactController, deleteContactController, patchContactController} from '../controllers/contacts.js';
+import  {getContactByIdController, getContactsController, createContactController, deleteContactController, patchContactController, upsertContactController} from '../controllers/contacts.js';
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
+import { isValidId } from '../validation/isValidId.js';
 
 
 const router = Router();
 
 router.get('/contacts',ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 
-router.post('/contacts', ctrlWrapper(createContactController));
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
+router.delete('/contacts/:contactId', isValidId,  ctrlWrapper(deleteContactController));
+router.put('/contacts/:contactId', validateBody(createContactSchema),ctrlWrapper(upsertContactController));
+
+router.patch('/contacts/:contactId', validateBody(updateContactSchema),ctrlWrapper(patchContactController));
 
 export default router;
 
